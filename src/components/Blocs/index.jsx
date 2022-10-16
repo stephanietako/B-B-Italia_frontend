1523eb8d3a7b04fb462d11bc49f9dc91547eaf7a
stephanietako <gabeshine@live.fr>
2022-10-16 15:02:21 +0200

    add blocs components

---
+++ src/components/Blocs/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const index = (img) => {
+  return (
+    <div className={styles.blocs}>
+      <div className={styles.blocs__content}>
+        <p>BLOC CONTENT</p>
+      </div>
+    </div>
+  );
+};
+
+export default index;

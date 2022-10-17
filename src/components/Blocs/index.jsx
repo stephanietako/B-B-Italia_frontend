8d27ae525b2b7f0dcedf994c5c44a88a04c9892b
stephanietako <gabeshine@live.fr>
2022-10-17 12:27:15 +0200

    refacto

--- src/components/Blocs/index.jsx
+++ src/components/Blocs/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
-const index = (props) => {
+const Blocs = ({ img, alt, text }) => {
   return (
     <div className={styles.blocs}>
       <div className={styles.blocs__content}>
         {/* <p>BLOC CONTENT</p> */}
         <div className={styles.blocs__img}>
-          <img src={props.img} alt={props.alt} />
+          <img src={img} alt={alt} />
 
           <div className={styles.blocs__text}>
-            <p>{props.text}</p>
+            <p>{text}</p>
           </div>
         </div>
       </div>
@@ -18,4 +18,4 @@ const index = (props) => {
   );
 };
 
-export default index;
+export default Blocs;

3ab2d8be047027dcd8723dcd2671857c5303a258
stephanietako <gabeshine@live.fr>
2022-10-17 18:59:20 +0200

    wip: components & add about component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import logo from "../../assets/logo/bb-logo-noir.png";
 
 const About = ({ img, alt, text }) => {
   return (
     <div className={styles.about}>
       <div className={styles.about__content}>
         <div className={styles.about__img}>
+          <span id={styles.about__christophe}>
+            <p>CHRISTOPHE</p>
+          </span>
           <img src={img} alt={alt} />
           <div className={styles.about__text}>
+            <img src={logo} alt={alt} />
             <p>{text}</p>
           </div>
         </div>

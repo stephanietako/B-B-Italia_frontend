8bfc7675fe17ea6c6faaeaa07606af660d23b867
stephanietako <gabeshine@live.fr>
2022-10-19 14:57:45 +0200

    added headband component

---
+++ src/components/Headband/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+const Headband = () => {
+  return (
+    <div className={styles.headband}>
+      <div className={styles.headband__content}>
+        <p>HELLO WORLD HELLO WORLD HELLO WORLD</p>
+      </div>
+    </div>
+  );
+};
+
+export default Headband;

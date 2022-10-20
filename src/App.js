3fd9fd80a21920ef9c85857f4291715f14894e99
stephanietako <gabeshine@live.fr>
2022-10-20 02:42:16 +0200

    modified Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/Home";
+// import About from "./components/About";
+// import Blocs from "./components/Blocs";
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
+          <Route exact path="/about" element={HomePage} />
+          <Route exact path="/products" element={HomePage} />
+          <Route exact path="/projects" element={HomePage} />
+          <Route exact path="/contact" element={HomePage} />
         </Route>
       </Routes>
     </div>

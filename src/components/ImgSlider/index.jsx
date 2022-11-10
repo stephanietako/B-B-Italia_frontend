1f362f953f47a063fa253c13fb9068df2399a3d7
stephanietako <gabeshine@live.fr>
2022-11-10 02:09:43 +0100

    wip: components ImgSlider

--- src/components/ImgSlider/index.jsx
+++ src/components/ImgSlider/index.jsx
@@ -4,20 +4,32 @@ import arrowLeft from "../../assets/icon/arrowLeft.svg";
 import arrowRight from "../../assets/icon/arrowRight.svg";
 import styles from "./styles.module.scss";
 
-const ImgSlider = (slides) => {
-  const [current, setcurrrent] = useState(0);
+const ImgSlider = ({ slides }) => {
+  const [current, setCurrrent] = useState(0);
   const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrrent(current === length - 1 ? 0 : current + 1);
+  };
+  console.log(current);
+
+  if (!Array.isArray(slides) || length <= 0) {
+    return null;
+  }
+
   return (
     <section className={styles.slider}>
       <div className={styles.__arrow_left}>
         <img src={arrowLeft} alt={"arrow left symbol"} />
       </div>
       <div className={styles.__arrow_right}>
-        <img src={arrowRight} alt={"arrow right symbol"} />
+        <img src={arrowRight} alt={"arrow right symbol"} onClick={nextSlide} />
       </div>
 
       {SliderData.map((slide, index) => {
-        return <img src={slide.image} alt="design" />;
+        return (
+          <img src={slide.image} alt="design" className={styles.__image} />
+        );
       })}
     </section>
   );

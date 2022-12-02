f0bedbfa7ae32530a58c4d2438e1f7f8854d145e
stephanietako <gabeshine@live.fr>
2022-12-02 19:49:24 +0100

    feature: add scroll to bottom

---
+++ src/components/ScrollToBottom/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+// Icons
+import chevron_double_down from "../../assets/icon/chevron_double_down.svg";
+
+// Style
+import styles from "./styles.module.scss";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(true);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 0) {
+      setVisible(false);
+    } else if (scrolled <= 0) {
+      setVisible(true);
+    }
+  };
+
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+  return (
+    <div className={styles.bottomToBtn}>
+      <div className={styles.__icon_position}>
+        <img
+          className={styles.__icon_style}
+          src={chevron_double_down}
+          alt={"scroll down symbol"}
+          onClick={scrollToBottom}
+          style={{ display: visible ? "inline" : "none" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ScrollButton;

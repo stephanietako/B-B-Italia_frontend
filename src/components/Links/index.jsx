d91319230800d5c5557d9c8cb57de3837742aa7c
stephanietako <gabeshine@live.fr>
2022-12-30 19:16:02 +0100

    wip: Navbar's links for desktop & mobile

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -5,37 +5,37 @@ import styles from "./styles.module.scss";
 const Links = ({ link, sublinks, dropdownPosition }) => {
   const { name, target, dropdown } = link;
   const [isOpen, setIsOpen] = useState(true);
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  // const [click, setClick] = useState(false);
+  // const handleClick = () => setClick(!click);
+  // const closeMobileMenu = () => setClick(false);
   ////////////////:
-  const [menuVisible, setMenuVisible] = useState(false);
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
+  // const [menuVisible, setMenuVisible] = useState(false);
+  // const toggleMenu = () => {
+  //   setMenuVisible(!menuVisible);
+  // };
+  const [showMenu, setShowMenu] = useState(false);
+
   const sublinksrender = sublinks[dropdownPosition]?.map((link) => (
     <li key={uuidv4()}>
-      <a href={link.target} onMouseLeave={closeMobileMenu}>
-        {link.name}
-      </a>
+      <a href={link.target}>{link.name}</a>
     </li>
   ));
   if (dropdown) {
     return (
-      <details open={isOpen} className={styles.dropdown} onClick={handleClick}>
-        <summary onClick={toggleMenu}>{name}</summary>
-        <summary onMouseLeave={toggleMenu}>{name}</summary>
-        <ul className={menuVisible ? "menu-visible" : "menu-hidden"}>
-          {sublinksrender}
-        </ul>
+      <details
+        open={isOpen}
+        className={styles.dropdown}
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <summary>{name}</summary>
+        {/* <summary onMouseLeave={toggleMenu}>{name}</summary> */}
+        {showMenu && <ul>{sublinksrender}</ul>}
       </details>
     );
   } else {
     return (
       <li className={styles.link}>
-        <a href={target} onClick={() => setIsOpen(!isOpen)}>
-          {name}
-        </a>
+        <a href={target}>{name}</a>
       </li>
     );
   }

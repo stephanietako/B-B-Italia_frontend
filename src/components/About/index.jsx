5be8685b1563f336784f24494d1de9a081fbcd64
stephanietako <gabeshine@live.fr>
2023-01-07 17:32:07 +0100

    fixed

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,9 +9,6 @@ const About = ({ img, alt, text }) => {
         <div className={styles.__img}>
           <img src={img} alt={alt} />
         </div>
-        {/* <span className={styles.__christophe}>
-            <p>CHRISTOPHE Papa</p>
-          </span> */}
         <div className={styles.__about_content}>
           <div className={styles.__logo}>
             <img src={logo} alt=" logo B&B Italia Saint-Tropez" />

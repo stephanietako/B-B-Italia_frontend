7e8937d192ea1649a121e3d53e000ba15b8e59ba
stephanietako <gabeshine@live.fr>
2023-02-09 16:10:51 +0100

    refacto

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -7,7 +7,7 @@ const Contact = ({ address, phone, mail }) => {
     <div className={styles.contact}>
       <div className={styles.__contact_container}>
         <div className={styles.__text_box}>
-          <h3>B&B Italia Saint-Tropez</h3>
+          <h2>B&B Italia Saint-Tropez</h2>
           <p>{address}</p>
           <p>{phone}</p>
           <p>{mail}</p>
